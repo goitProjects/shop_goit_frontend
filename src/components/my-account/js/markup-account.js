42d21c8a158c27a61a0c17fa016482b4a1e53ec6
Daniel Tsvirkun <danila.tsvirkun555@gmail.com>
2021-02-23 19:51:05 +0200

    ru version

---
+++ src/components/my-account/js/markup-account.js
@@ -0,0 +1,94 @@
+import axios from 'axios';
+import { refs } from './refs';
+import hbsUserInfo from '../templates/account-user-info.hbs';
+import hbsFavoritesGoods from '../templates/favorites-goods-modal.hbs';
+import myAds from '../templates/my-ads-modal.hbs';
+import { removeFavorites, removeUserAds } from './counter-goods';
+import { api } from '../../services/api';
+
+function murkupUserInfo(data) {
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    refs.mobileBackdropMyAccount.innerHTML = hbsUserInfo(data);
+  } else {
+    refs.modalBackdropMyAccount.innerHTML = hbsUserInfo(data);
+  }
+}
+
+async function murkupFavoritesGoods() {
+  const parseFavorites = JSON.parse(localStorage.getItem('user-info'))
+    .favorites;
+  const favCheckBtn = document.querySelector('.account-list__favorites');
+  favCheckBtn.addEventListener('click', checkFavs);
+  api.filterMyAccount(parseFavorites).then(res => {
+    if (window.matchMedia('(max-width: 767px)').matches) {
+      refs.mobileBackdropFavorites.innerHTML = hbsFavoritesGoods(res);
+    } else {
+      refs.modalBackdropFavorites.innerHTML = hbsFavoritesGoods(res);
+    }
+    removeFavorites();
+  });
+  const favoritesValue = document.querySelector('.js-counter__heart');
+  favoritesValue.textContent = parseFavorites.length;
+
+  async function checkFavs() {
+    let call;
+    let filteredFavorites = [];
+
+    const token = JSON.parse(localStorage.getItem('user-info')).token;
+
+    const info = await axios({
+      method: 'GET',
+      url: 'https://callboard-back-presentational.goit.global/user',
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    const parseFavorites = info.data.favourites;
+
+    if (parseFavorites[0]) {
+      for (let i = 0; i < parseFavorites.length; i++) {
+        call = await axios({
+          method: 'GET',
+          url: `https://callboard-back-presentational.goit.global/call/${parseFavorites[i]._id}`,
+          headers: { Authorization: token },
+        });
+        if (call.data.success) {
+          filteredFavorites.push(parseFavorites[i]._id);
+        } else {
+          const favs = Array.from(
+            document.querySelectorAll('.selected-goods__item'),
+          );
+          // const doc = favs.find(item => item.dataset.id === parseFavorites[i]);
+          // doc.remove();
+          favoritesValue.textContent -= 1;
+        }
+      }
+    }
+    localStorage.setItem(
+      'user-info',
+      JSON.stringify({
+        userId: info.data.id,
+        email: info.data.email,
+        token,
+        favorites: filteredFavorites,
+        adv: info.data.calls.map(item => item._id),
+      }),
+    );
+  }
+}
+
+function murkupMyAds() {
+  const parseMyAds = JSON.parse(localStorage.getItem('user-info')).adv;
+  api.filterMyAccount(parseMyAds).then(res => {
+    if (window.matchMedia('(max-width: 767px)').matches) {
+      refs.mobileBackdropMyAds.innerHTML = myAds(res);
+    } else {
+      refs.modalBackdropMyAds.innerHTML = myAds(res);
+    }
+    removeUserAds();
+  });
+  const myAdsValue = document.querySelector('.js-counter__add');
+  myAdsValue.textContent = parseMyAds.length;
+}
+export { murkupFavoritesGoods, murkupMyAds, murkupUserInfo };
---
+++ src/components/section-categories/categories.js
@@ -0,0 +1,224 @@
+import './categories-styles/category.css';
+import './categories-styles/more-info.css';
+import Siema from 'siema';
+import catMain from './categories-templates/category-main.hbs';
+import catPop from './categories-templates/category-item.hbs';
+import { load, ready } from '../loader/loader';
+import { api } from '../services/api';
+import { showItemModal } from '../item-modal/item-modal-open';
+import throttle from 'lodash.throttle';
+import data from '../services/data';
+// =========================================================
+const blockList = document.querySelector('.block__list');
+const arroundBlockList = document.querySelector('.arround-block__list');
+const horizontalBlock = document.querySelector('.horizontal-block');
+// =========================================================
+const categories = document.querySelector('.categories .container');
+const btnLoadMore = document.querySelector('.load-more');
+let counterStartIdx = 0;
+let counterEndIdx = 2;
+const nameAllCategories = [
+  'electronics',
+  'property',
+  'transport',
+  'work',
+  'businessAndServices',
+  'recreationAndSport',
+  'free',
+  'trade',
+];
+load();
+function fnSwitch(startIdx, endIdx) {
+  nameAllCategories.slice(startIdx, endIdx).forEach((word, idx, curArr) => {
+    if (counterStartIdx < nameAllCategories.length + 1) {
+      counterStartIdx += 1;
+    }
+
+    if (curArr.length < 2 || counterEndIdx !== 3) {
+      counterEndIdx += 1;
+    }
+    return test(word);
+  });
+}
+
+export function test(word) {
+  return api.getCategory(word).then(data => {
+    if (document.querySelector('.loader-wrapper')) {
+      ready();
+      blockList.classList.add('block__list-show');
+      arroundBlockList.classList.add('arround-block__list-show');
+      horizontalBlock.classList.add('horizontal-block-show');
+    }
+    switch (word) {
+      case 'property':
+        data[0].nameCategory = 'Недвижимость';
+        data[0].descriptionCategory =
+          'При публикации объявлений в разделе Недвижимость необходимо придерживаться правил, что и при размещении любых других объявлений. Однако есть несколько нюансов, на которые стоит обратить внимание.';
+        break;
+      case 'transport':
+        data[0].nameCategory = 'Транспорт';
+        data[0].descriptionCategory =
+          'В этом разделе вы можете найти любое передвигающееся средство по вашему вкусу';
+        break;
+      case 'work':
+        data[0].nameCategory = 'Работа';
+        data[0].descriptionCategory =
+          'Если ищешь работу  тогда тебе к нам. более 500 вакансий каждый день';
+        break;
+      case 'electronics':
+        data[0].nameCategory = 'Электроника';
+        data[0].descriptionCategory =
+          'Любая электроника от детских игрушек до холодильников';
+        break;
+      case 'businessAndServices':
+        data[0].nameCategory = 'Бизнес и услуги';
+        data[0].descriptionCategory =
+          'Нужна помощь в продвижении малого бизнеса? Торопись тебе точно к нам';
+        break;
+      case 'recreationAndSport':
+        data[0].nameCategory = 'Отдых и спортивные состязания';
+        data[0].descriptionCategory =
+          'Ищешь место куда бы укрыться от суеты и будней? Мы покажем тебе место о котором ты мечтал';
+        break;
+      case 'free':
+        data[0].nameCategory = 'Бесплатно';
+        data[0].descriptionCategory =
+          'Забери меня скорей. Отдадут меня быстрей';
+        break;
+      case 'trade':
+        data[0].nameCategory = 'Обмен';
+        data[0].descriptionCategory =
+          'Хочешь обновку а денег нет. Кто ищет всегда найдет';
+        break;
+      default:
+        break;
+    }
+    let category = data[0].category;
+    categories.insertAdjacentHTML('beforeend', catMain(data[0]));
+    let list = document.querySelector(`.${category}-list`);
+    list.insertAdjacentHTML('beforeend', catPop(data));
+    const slidePrev = document.querySelector(
+      `.${category}-wrapper .slide-prev`,
+    );
+    const slideNext = document.querySelector(
+      `.${category}-wrapper .slide-next`,
+    );
+    // ===================================================
+
+    if (window.matchMedia('(max-width: 767px)').matches) {
+      const mySiema = new Siema({
+        selector: list,
+        loop: true,
+        duration: 100,
+        perPage: 1,
+        easing: 'cubic-bezier(0.250, 0.250, 0.750, 0.750)',
+      });
+      window.addEventListener(
+        'resize',
+        throttle(() => {
+          if (
+            window.matchMedia('(min-width: 768px)').matches &&
+            window.matchMedia('(max-width: 1279px)').matches
+          ) {
+            mySiema.perPage = 2;
+            mySiema.loop = false;
+            mySiema.config.perPage = 2;
+            mySiema.config.loop = false;
+          } else if (window.matchMedia('(min-width: 1280px)').matches) {
+            mySiema.perPage = 4;
+            mySiema.loop = false;
+            mySiema.config.perPage = 4;
+            mySiema.config.loop = false;
+          } else if (window.matchMedia('(max-width: 767px)').matches) {
+            mySiema.perPage = 1;
+            mySiema.loop = true;
+            mySiema.config.perPage = 1;
+            mySiema.config.loop = true;
+          }
+        }, 300),
+      );
+    } else if (
+      window.matchMedia('(min-width: 768px)').matches &&
+      window.matchMedia('(max-width: 1279px)').matches
+    ) {
+      const mySiemaTablet = new Siema({
+        selector: list,
+        duration: 200,
+        perPage: 2,
+      });
+      slidePrev.addEventListener('click', () => mySiemaTablet.prev());
+      slideNext.addEventListener('click', () => mySiemaTablet.next());
+      window.addEventListener(
+        'resize',
+        throttle(() => {
+          if (window.matchMedia('(max-width: 767px)').matches) {
+            mySiemaTablet.perPage = 1;
+            mySiemaTablet.loop = true;
+            mySiemaTablet.config.perPage = 1;
+            mySiemaTablet.config.loop = true;
+          } else if (window.matchMedia('(min-width: 1280px)').matches) {
+            mySiemaTablet.perPage = 4;
+            mySiemaTablet.loop = false;
+            mySiemaTablet.config.perPage = 4;
+            mySiemaTablet.config.loop = false;
+          } else if (
+            window.matchMedia('(min-width: 768px)').matches &&
+            window.matchMedia('(max-width: 1279px)').matches
+          ) {
+            mySiemaTablet.perPage = 2;
+            mySiemaTablet.loop = false;
+            mySiemaTablet.config.perPage = 2;
+            mySiemaTablet.config.loop = false;
+          }
+        }, 300),
+      );
+    } else if (window.matchMedia('(min-width: 1280px)').matches) {
+      const mySiemaPC = new Siema({
+        selector: list,
+        duration: 200,
+        perPage: 4,
+      });
+      slidePrev.addEventListener('click', () => mySiemaPC.prev());
+      slideNext.addEventListener('click', () => mySiemaPC.next());
+      window.addEventListener(
+        'resize',
+        throttle(() => {
+          if (window.matchMedia('(max-width: 767px)').matches) {
+            mySiemaPC.perPage = 1;
+            mySiemaPC.loop = true;
+            mySiemaPC.config.perPage = 1;
+            mySiemaPC.config.loop = true;
+          } else if (
+            window.matchMedia('(min-width: 768px)').matches &&
+            window.matchMedia('(max-width: 1279px)').matches
+          ) {
+            mySiemaPC.perPage = 2;
+            mySiemaPC.loop = false;
+            mySiemaPC.config.perPage = 2;
+            mySiemaPC.config.loop = false;
+          } else if (window.matchMedia('(min-width: 1280px)').matches) {
+            mySiemaPC.perPage = 4;
+            mySiemaPC.loop = false;
+            mySiemaPC.config.perPage = 4;
+            mySiemaPC.config.loop = false;
+          }
+        }, 300),
+      );
+    }
+    const ulX = document.querySelector(`.${word}-list`);
+    showItemModal(ulX);
+    btnLoadMore.classList.remove('hide');
+    btnLoadMore.addEventListener('click', showMoreCategories);
+    if (counterStartIdx === nameAllCategories.length) {
+      btnLoadMore.classList.add('hide');
+    }
+  });
+}
+fnSwitch(counterStartIdx, counterEndIdx);
+
+function showMoreCategories(e) {
+  if (counterStartIdx > nameAllCategories.length) {
+    return;
+  }
+  fnSwitch(counterStartIdx, counterEndIdx);
+}
