22ae9f6677d6179d67e3ea484d0f6f298d4fbe3f
z9877969 <z9877969@ukr.net>
2022-10-05 17:43:55 +0300

    replaces ru to parcel

---
+++ src/components/services/data.js
@@ -0,0 +1,12 @@
+export default {
+  allCategories: [],
+  property: [],
+  transport: [],
+  work: [],
+  electronics: [],
+  businessAndServices: [],
+  recreationAndSport: [],
+  free: [],
+  trade: [],
+  advertisement: [],
+};
